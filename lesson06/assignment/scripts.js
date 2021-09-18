7d7822c4bb654dc32e83ac9abc0cdd3e26d39e7d
DBalint88 <dbalint88@gmail.com>
2021-09-18 16:31:43 -0400

    added comments and updated all footers

--- lesson06/assignment/scripts.js
+++ lesson06/assignment/scripts.js
@@ -1,10 +1,14 @@
+
+// Make selectors for the critical elemnts, the viewer (embed tag) and the song buttons.
 let viewer = document.getElementById('pdf-viewer');
 let buttons = document.getElementsByClassName('song-button');
 
+// When a song button is click, take its ID and make it the file path for the viewer's src attribute.
 let imageLoader = function() {
   viewer.src = "/images/pdfs/" + this.id + '.pdf'
 };
 
+// "buttons" above selects all of the buttons in an HTML object.  This loops through that object listening for clicks, then sends the clicked button to imageLoader
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', imageLoader);
 }
