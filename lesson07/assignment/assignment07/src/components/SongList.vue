bdb7bac14715f19dc044d1a424ff16fbe870a0fa
DBalint88 <dbalint88@gmail.com>
2021-11-08 15:29:58 -0500

    finally achieved song page functionality, w basic dynamic video link

--- lesson07/assignment/assignment07/src/components/SongList.vue
+++ lesson07/assignment/assignment07/src/components/SongList.vue
@@ -1,26 +1,114 @@
 <template>
-    <b-card no-body class="mb-1">
+  <div class="accordion col-lg-3 col-md-12 px-0 my-4" role="tablist" id="accordionExample">
+    <b-card v-for="level in levels" v-bind:key="level" no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle="'accordion-'+indexRef" variant="info">{{ name }}</b-button>
+        <b-button block v-b-toggle="'accordion-'+level.index" variant="info">{{ level.skillLevel }}</b-button>
       </b-card-header>
-      <b-collapse :id="'accordion-'+indexRef" accordion="my-accordion" role="tabpanel">
+      <b-collapse :id="'accordion-'+level.index" accordion="my-accordion" role="tabpanel">
         <b-card-body>
           <ol>
-            <li v-for="song in songs" v-bind:key="song" class="song-button" :id="song.pdfLink" :data-video="song.videoLink" @click="pdfURL=song.pdfLink">{{ song.title }}</li>
+            <li v-for="song in level.songs" v-bind:key="song" class="song-button" :data-pdf="'./sheet-music/'+song.pdf+'.pdf'" :data-video="song.video" @click="updateDisplay">{{ song.title }}</li>
           </ol>
         </b-card-body>
       </b-collapse>
     </b-card>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: "SongList",
-  props: {
-    name: String,
-    indexRef: String,
-    songs: Array
+  methods: {
+    updateDisplay(e) {
+      this.songSheet = e.currentTarget.getAttribute('data-pdf');
+      this.songVideo = e.currentTarget.getAttribute('data-video');
+      this.$emit("sheetEmit", this.songSheet);
+      this.$emit("vidEmit", this.songVideo);
+      },
   },
+  name: "SongList",
+  data() {
+      return {
+        songSheet: '',
+        songVideo: '',
+        levels: [
+          {
+            skillLevel: 'Level 1',
+            index: '1',
+            songs: [
+              {
+                title: 'Hot Cross Buns',
+                pdf: '01-hot-cross-buns',
+                video: 'https://youtu.be/PaaAvBGLem0'
+              },
+              {
+                title: 'Mary Had A Little Lamb',
+                pdf: '01-mary-had-a-little-lamb',
+                video: 'https://youtu.be/MX2irXhbnCs'
+              },
+              {
+                title: 'Au Clair de la Lune',
+                pdf: '01-au-clair-de-la-lune',
+                video: 'https://youtu.be/KF8lZntKpSs'
+              },
+              {
+                title: 'Jingle Bells',
+                pdf: '01-jingle-bells',
+                video: 'https://youtu.be/-FtdwRM0zZY'
+              },
+              {
+                title: 'Rain, Rain, Go Away',
+                pdf: '01-rain-rain',
+                video: 'https://youtu.be/SOgK23KkYJs'
+              },
+              {
+                title: 'A Tisket, A Tasket',
+                pdf: '01-tisket-tasket',
+                video: 'https://youtu.be/fEXYaA0sW5M'
+              },
+              {
+                title: 'Ode to Joy',
+                pdf: '01-ode-to-joy',
+                video: 'https://youtu.be/zA5IbQj2Upk'
+              },
+              {
+                title: 'Sweetly Sings the Donkey',
+                pdf: '01-sweetly-sings-the-donkey',
+                video: 'https://youtu.be/0lwKqvAa0lw'
+              },
+              {
+                title: 'Go Tell Aunt Rhodie',
+                pdf: '01-go-tell-aunt-rhodie',
+                video: 'https://youtu.be/5E65FgTD924'
+              },
+              {
+                title: 'Banks of the Ohio',
+                pdf: '01-banks-of-the-ohio',
+                video: 'https://youtu.be/534fljXmmF8'
+              }
+            ],
+          },
+          {
+            skillLevel: 'Level 2',
+            index: '2',
+            songs: [
+              {
+                title: 'Pop! Goes the Weasel',
+                pdf: '02-pop-goes-the-weasel',
+                video: 'https://youtu.be/tsCWF9hl5IY'
+              }
+            ]
+          },
+          {
+            skillLevel: 'Level 3',
+            index: '3'
+          },
+          {
+            skillLevel: 'Level 4',
+            index: '4'
+          },
+        ],
+      }
+    }
 };
 </script>
--- lesson07/assignment/assignment07/src/views/Songs.vue
+++ lesson07/assignment/assignment07/src/views/Songs.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="sidebar-main" class="row bg-dark mx-0 py-2">
-    <button type="button" name="button" @click="updateDisplay">Click me</button>
-    <div class="accordion col-lg-3 col-md-12 px-0 my-4" role="tablist" id="accordionExample">
-      <SongList v-for="level in levels" :name="level.skillLevel" :indexRef="level.index" :songs="level.songs" v-bind:key="level"></SongList>
+  <div id="sidebar-main" class="row bg-dark mx-0 py-2" >
 
-    </div>
+    <SongList @sheetEmit="updateDisplay" @vidEmit="updateVidLink"/>
 
 
 
     <!-- The PDF viewer.  Not sure how to use objects/iframes, embed seemed easy enough.  JS loops through the .song-button's above and listens for a click, then updates the src attribute of the embed with the correct file path. -->
+
     <main id="song-main" class="col-lg-9 col-md-12">
-      <embed id="pdf-viewer" type="application/pdf" :src="pdfURL">
+      <embed id="pdf-viewer" type="application/pdf" :src="this.selectedSongSheet">
+
     </main>
+    <a :href="this.selectSongVideo"><button type="button" name="button" :href="this.selectedSongVideo">click a the button</button></a>
   </div>
 </template>
 
@@ -23,99 +23,24 @@ import SongList from "@/components/SongList.vue";
 
 export default {
   name: "Songs",
-  components: {
-    SongList
+  data() {
+    return {
+      selectedSongSheet: "",
+      selectedSongVideo: ""
+    }
   },
   methods: {
-    updateDisplay: function () {
-      this.pdfURL = "./sheet-music/01-tisket-tasket.pdf"
+    updateDisplay(e) {
+      this.selectedSongSheet = e;
+      // this.selectSong = e.songData;
+    },
+    updateVidLink(e) {
+      this.selectSongVideo = e;
     }
   },
-  data() {
-      return {
-        pdfURL: '',
-        levels: [
-          {
-            skillLevel: 'Level 1',
-            index: '1',
-            songs: [
-              {
-                title: 'Hot Cross Buns',
-                pdf: './sheet-music/01-hot-cross-buns.pdf',
-                video: 'https://youtu.be/PaaAvBGLem0'
-              },
-              {
-                title: 'Mary Had A Little Lamb',
-                pdf: '01-mary-had-a-little-lamb',
-                video: 'https://youtu.be/MX2irXhbnCs'
-              },
-              {
-                title: 'Au Clair de la Lune',
-                pdf: '01-au-clair-de-la-lune',
-                video: 'https://youtu.be/KF8lZntKpSs'
-              },
-              {
-                title: 'Jingle Bells',
-                pdf: '01-jingle-bells',
-                video: 'https://youtu.be/-FtdwRM0zZY'
-              },
-              {
-                title: 'Rain, Rain, Go Away',
-                pdf: '01-rain-rain',
-                video: 'https://youtu.be/SOgK23KkYJs'
-              },
-              {
-                title: 'A Tisket, A Tasket',
-                pdf: '01-tisket-tasket',
-                video: 'https://youtu.be/fEXYaA0sW5M'
-              },
-              {
-                title: 'Ode to Joy',
-                pdf: '01-ode-to-joy',
-                video: 'https://youtu.be/zA5IbQj2Upk'
-              },
-              {
-                title: 'Sweetly Sings the Donkey',
-                pdf: '01-sweetly-sings-the-donkey',
-                video: 'https://youtu.be/0lwKqvAa0lw'
-              },
-              {
-                title: 'Go Tell Aunt Rhodie',
-                pdf: '01-go-tell-aunt-rhodie',
-                video: 'https://youtu.be/5E65FgTD924'
-              },
-              {
-                title: 'Banks of the Ohio',
-                pdf: '01-banks-of-the-ohio',
-                video: 'https://youtu.be/534fljXmmF8'
-              }
-            ],
-          },
-          {
-            skillLevel: 'Level 2',
-            index: '2',
-            songs: [
-              {
-                title: 'Pop! Goes the Weasel',
-                pdf: '02-pop-goes-the-weasel',
-                video: 'https://youtu.be/tsCWF9hl5IY'
-              }
-            ]
-          },
-          {
-            skillLevel: 'Level 3',
-            index: '3'
-          },
-          {
-            skillLevel: 'Level 4',
-            index: '4'
-          },
-        ],
-
-
-
-      }
-    }
+  components: {
+    SongList
+  }
 };
 </script>
 
