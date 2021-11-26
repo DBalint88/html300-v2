d814a8372582d43870b34f9092a10e458945c549
DBalint88 <dbalint88@gmail.com>
2021-11-26 15:49:30 -0500

    assignment09.  API functional, Discovery page complete

--- lesson08/assignment/assignment08/src/views/Discover.vue
+++ lesson08/assignment/assignment08/src/views/Discover.vue
@@ -1,31 +1,86 @@
 <template>
   <main class="bg-dark text-primary py-2">
-    <h2>Coming soon:</h2>
-    <p>On this page I will build an API interface using <a href="https://tastedive.com/">TasteDive</a> to help students discover more music they might like based on what other people listen to who like the same artists.  Stay tuned!  For now, enjoy an if/else button:</p>
 
-    <div class="button-game">
-      <!-- There wasn't a great reason to use v-else on any of my other pages, so I've just included it here to demonstrate I get the concept.-->
-      <button v-if="status === true" class="btn btn-primary" type="button" name="button" @click="toggle">Click this blue button!</button>
-      <button v-else class="btn btn-danger" type="button" name="button" @click="toggle">Click this red button!</button>
+    <h2>Use this page to discover new artists you might like.</h2>
+    <p>Enter your favorite artist's name below to begin.</p>
+
+    <div class="disover-form my-5">
+      <label for="artist">Artist:
+        <input v-model="input" type="text" name="artist" @keyup.enter="callAPI" placeholder="Type in an artist's name..." >
+      </label>
+      <button type="button" name="button" class="btn btn-success" @click="callAPI">Submit</button>
+    </div>
+
+    <div v-if="!this.loading && this.suggestions.Similar.Results.length != 0">
+      <h2 v-if="this.suggestions">Showing search results for "{{ suggestions.Similar.Info[0].Name }}":</h2>
+
+      <DiscoverCard :suggestions="this.suggestions"/>
 
     </div>
 
+    <div v-if="!this.loading && this.suggestions.Similar.Results.length == 0" class="discoverror">
+      <h6>Sorry, no search results.</h6>
+      <p>This might be because you've misspelled the artist's name.  Please try again.</p>
+    </div>
 
   </main>
 </template>
 
 <script>
+import axios from 'axios'
+import DiscoverCard from "@/components/DiscoverCard.vue"
 export default {
   name: "Discover",
-  methods: {
-    toggle() {
-      this.status = !this.status;
-    }
+  components: {
+    DiscoverCard
   },
   data() {
     return {
-      status: true
+      suggestions: null,
+      loading: true,
+      errored: false,
+      input: null,
+    }
+  },
+
+  methods: {
+    callAPI() {
+      axios
+        .get('https://tastedive.com/api/similar?q=' + this.input + '&type=music&info=1&limit=10&k=420205-DanielBa-UVUVOXKE')
+        .then(response => (this.suggestions = response.data))
+        .catch(error => {
+          console.error(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
     }
   }
 };
 </script>
+
+<style>
+
+.discoverCard {
+  border-radius: 1rem;
+}
+
+.YouTube-container {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.iframe-wrapper {
+  position: relative;
+  padding-top: 56%
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
